2628073fdc27e7a53e1a4a0e1f467f138b26c1b0
Frank <tianfrank@gmail.com>
2024-03-08 10:26:38 -0500

    Preliminary node and character classes

---
+++ Assets/Code/Character.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+//Simple parent class for both player and enemy characters
+public class Character
+{
+	//stores position of character
+	public PosNode pos;
+	//we will probably need to add more stuff as it comes up, but it's good to have a way of adding properties
+	//to all characters together or players and enemies seperately I think
+
+
+	//empty init
+	public Character()
+	{
+		pos = null;
+	}
+
+    //nonempty init
+    public Character(PosNode p)
+    {
+		pos = p;
+    }
+}
+
+
+//player class, feel free to change name btw
+public class Player : Character
+{
+
+
+    public Player(PosNode p : base(p))
+    {
+    }
+
+
+    //move the player north one node
+    public void moveNorth()
+    {
+        if (this.pos.getNorth().isOpen())
+        {
+            this.pos.setOccupant(null);
+            this.pos = this.pos.getNorth();
+            this.pos.setOccupant(this);
+
+        }
+    }
+}
+
+
+//Parent class for all enemies, we can create children classes for each subtype
+public class Enemy : Character
+{
+    public Enemy(PosNode p : base(p))
+    {
+    }
+}
+
---
+++ Assets/Code/PosNode.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+
+//Simple node to store the position in a grid
+public class PosNode
+{
+
+	//stores the position of the node
+	private (int, int) pos;
+	//stores the character occupying the node
+	public Character Occupant;
+	//stores the nodes north (+y), south (-y), east (+x), and west (-x) of this node, may need to add more or modify to an array if we add diagonal movement
+	private PosNode North;
+    private PosNode South;
+    private PosNode East;
+    private PosNode West;
+
+	//empty init
+	public PosNode()
+	{
+		pos = (0, 0);
+		Occupant = null;
+		North = null;
+		South = null;
+		East = null;
+		West = null;
+	}
+
+	//nonempty init
+	public PosNode((int, int) p, Character o, PosNode n, PosNode s, PosNode e, PosNode w)
+	{
+		pos = pos;
+		Occupant = Occupant;
+		North = n;
+		South = s;
+		East = e;
+		West = w;
+	}
+
+	//getter methods
+	public Character getOcc()
+	{
+		return Occupant;
+	}
+
+	public PosNode getNorth()
+	{
+		return North:
+	}
+    public PosNode getSouth()
+    {
+        return South:
+	}
+    public PosNode getEast()
+    {
+        return East:
+	}
+    public PosNode getWest()
+    {
+        return West:
+	}
+
+	public void setOccupant(Character c)
+	{
+		Occupant = c;
+	}
+
+	//return if node is occupied
+	public bool isOpen()
+	{
+		return (Occupant == null);
+	}
+
+}
